2c132f4046ef698c51d9b19a384e2a09f1e6c2f9
Becka <rebeccaellicehsiao@gmail.com>
2018-05-14 19:51:28 -0700

    Add RadioButton that correspond to Offer or Request postType
    This is the component for the MakeEventVC toggle that will be added later.
    If one is currently editing a post of type request, the request radio button will be active while the offer radio button will be inactive.
    Likewise in the case that offer is the one active, the request radio button will be inactive.
    This is just the setup (something in MakeEventVC will have to configure it later)

--- omni/View/RadioButton.swift
+++ omni/View/RadioButton.swift
@@ -6,4 +6,31 @@
 //  Copyright © 2018年 Rebecca Hsiao. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class RadioButton: UIView {
+
+    @IBOutlet weak var dot: UIView!
+    @IBOutlet weak var label: UILabel!
+    
+    var active = false
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.dot.layer.cornerRadius = self.dot.bounds.width / 2.0
+        self.label.font = UIFont(name: "GothamPro", size: 13.0)
+    }
+
+    func configureAsRequest() {
+        self.dot.backgroundColor = self.active ? UIColor.request : UIColor.inactive
+        self.label.textColor = self.active ? UIColor.darkText : UIColor.inactive
+        self.label.text = "Request"
+    }
+
+    func configureAsOffer() {
+        self.dot.backgroundColor = self.active ? UIColor.offer : UIColor.inactive
+        self.label.textColor = self.active ? UIColor.darkText : UIColor.inactive
+        self.label.text = "Offer"
+    }
+
+}
