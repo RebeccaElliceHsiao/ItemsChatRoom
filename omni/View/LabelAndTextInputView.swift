95020ae27e0d8fb94d881a22459d159aea4a879e
Becka <rebeccaellicehsiao@gmail.com>
2018-05-14 19:57:43 -0700

    Add custom inputview and form view
    For LabelAndTextInputView, add functionality to configure for date and inselectable text.
    For PostForm, the fields are populated with stacks of labelAndTextInputView with the corresponding attributes from Post Model

--- omni/View/LabelAndTextInputView.swift
+++ omni/View/LabelAndTextInputView.swift
@@ -7,3 +7,240 @@
 //
 
 import Foundation
+import UIKit
+import Cartography
+
+class LabelAndTextInputView: UIView {
+    
+    @IBOutlet weak var textView: TextView!
+    @IBOutlet weak var label: UILabel!
+
+    var date: Date?
+    var flexibleHours: Bool? {
+        get {
+            return self.datePicker.datePickerMode == .date
+        }
+    }
+
+    private var datePicker: UIDatePicker!
+    private var picker: UIPickerView!
+    private var pickerDataSource: (UIPickerViewDataSource & UIPickerViewDelegate)!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.commonInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.commonInit()
+    }
+
+    private func commonInit() {
+        let nib = UINib(nibName: "LabelAndTextInputView", bundle: nil)
+        let view = nib.instantiate(withOwner: self, options: nil).first as! UIView
+        self.addSubview(view)
+
+        constrain(view, self) { (view, viewSelf) in
+            view.edges == viewSelf.edges
+        }
+
+        self.textView.isScrollEnabled = true
+
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 45))
+        let flexibleSeparator = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneButtonPressed))
+        toolbar.items = [flexibleSeparator, doneButton]
+        self.textView.inputAccessoryView = toolbar
+
+        self.label.font = UIFont.alloBoldFont(size: 9.0)
+        self.label.textColor = UIColor.alloGrayText
+    }
+
+    var required: Bool = false {
+        didSet {
+            configureLabelText()
+        }
+    }
+
+    var labelText: String = "" {
+        didSet {
+            configureLabelText()
+        }
+    }
+
+    var placeholder: String = "Select" {
+        didSet {
+            self.textView.placeholder = placeholder
+        }
+    }
+
+    var text: String {
+        get {
+            return self.textView.text
+        }
+        set {
+            self.textView.text = newValue
+            self.textView.delegate?.textViewDidChange?(self.textView)
+        }
+    }
+
+    var offset: Double {
+        get {
+            return self.textView.offset ?? 0
+        }
+        set {
+            self.textView.offset = newValue
+        }
+    }
+
+    func configureForNumericInput() {
+        self.textView.keyboardType = .numberPad
+    }
+
+    func configureForFreeformInput() {
+        self.textView.isScrollEnabled = true
+    }
+
+    func configureNotTypable() {
+        self.textView.isEditable = false
+    }
+
+    func configureForDateInput(withFlexibility: Bool = false) {
+        self.datePicker = UIDatePicker()
+        self.datePicker.datePickerMode = .date
+        self.textView.inputView = self.datePicker
+        self.textView.isScrollEnabled = false
+
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        if withFlexibility {
+            let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dateDoneButtonWithFlexibilityPressed))
+            let flexibleSeparator = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+            toolbar.items = [flexibleSeparator, doneButton]
+        } else {
+            let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dateDoneButtonPressed))
+            toolbar.items = [doneButton]
+        }
+        self.textView.inputAccessoryView = toolbar
+    }
+
+    func configureForDateAndTimeInput() {
+        self.datePicker = UIDatePicker()
+        self.datePicker.datePickerMode = .dateAndTime
+        self.textView.inputView = self.datePicker
+        self.textView.isScrollEnabled = false
+
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        let flexButton = UIBarButtonItem(title: "Specify Hours", style: .done, target: self, action: #selector(flexibleTimePressed))
+        let flexibleSeparator = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dateDoneButtonPressed))
+        toolbar.items = [flexButton, flexibleSeparator, doneButton]
+        self.textView.inputAccessoryView = toolbar
+    }
+
+    func configureForTimeInput() {
+        self.datePicker = UIDatePicker()
+        self.datePicker.datePickerMode = .time
+        self.datePicker.minuteInterval = 15
+        self.textView.inputView = self.datePicker
+        let dateComponents = DateComponents(calendar: Calendar.current, timeZone: nil, era: nil, year: nil, month: nil, day: nil, hour: 10, minute:0, second: 0, nanosecond: nil, weekday: nil, weekdayOrdinal: nil, quarter: nil, weekOfMonth: nil, weekOfYear: nil, yearForWeekOfYear: nil)
+        if let date = Calendar.current.date(from: dateComponents) {
+            self.datePicker.date = date
+        }
+
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 45))
+        let flexibleSeparator = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(timeDoneButtonPressed))
+        toolbar.items = [flexibleSeparator, doneButton]
+        self.textView.inputAccessoryView = toolbar
+    }
+
+    func configureForPicker(options: [String]) {
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(pickerDoneButtonPressed))
+        self.configureForPickerSettings(options: options, doneButton: doneButton)
+    }
+
+    func validate() -> Bool {
+        if !self.required {
+            return true
+        }
+        return self.textView.text.count > 0
+    }
+
+    private func configureForPickerSettings(options: [String], doneButton: UIBarButtonItem) {
+        self.picker = UIPickerView()
+        self.pickerDataSource = PickerDataSource(options: options)
+        self.picker.dataSource = self.pickerDataSource
+        self.picker.delegate = self.pickerDataSource
+        self.textView.inputView = self.picker
+        self.textView.isEditable = false
+        self.textView.isScrollEnabled = false
+
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 45))
+        let flexibleSeparator = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        toolbar.items = [flexibleSeparator, doneButton]
+        self.textView.inputAccessoryView = toolbar
+    }
+
+    @objc private func flexibleTimePressed(button: UIBarButtonItem) {
+        if self.datePicker.datePickerMode == .date {
+            button.title = "Flexible Hours"
+            self.datePicker.datePickerMode = .dateAndTime
+        } else {
+            button.title = "Specify Hours"
+            self.datePicker.datePickerMode = .date
+        }
+    }
+
+    @objc private func dateDoneButtonPressed() {
+        self.date = self.datePicker.date
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM/dd/yyyy"
+        self.textView.text = formatter.string(from: self.datePicker.date)
+        self.textView.delegate?.textViewDidChange?(self.textView)
+        self.textView.resignFirstResponder()
+    }
+
+    @objc private func dateDoneButtonWithFlexibilityPressed() {
+        self.date = self.datePicker.date
+        let formatter = DateFormatter()
+        if self.datePicker.datePickerMode == .date {
+            formatter.dateFormat = "E, MMM d"
+            self.textView.text = formatter.string(from: self.datePicker.date) + " flexible"
+        } else {
+            formatter.dateFormat = "E, MMM d hh:mm a"
+            self.textView.text = formatter.string(from: self.datePicker.date)
+        }
+        self.textView.delegate?.textViewDidChange?(self.textView)
+        self.textView.resignFirstResponder()
+    }
+
+    @objc private func timeDoneButtonPressed() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm a"
+        self.textView.text = formatter.string(from: self.datePicker.date)
+        self.textView.delegate?.textViewDidChange?(self.textView)
+        self.textView.resignFirstResponder()
+    }
+
+    @objc private func pickerDoneButtonPressed() {
+        let row = self.picker.selectedRow(inComponent: 0)
+        let title = self.pickerDataSource.pickerView!(self.picker, titleForRow: row, forComponent: 0)
+        self.textView.text = title
+        self.textView.delegate?.textViewDidChange?(self.textView)
+        self.textView.resignFirstResponder()
+    }
+
+    @objc private func doneButtonPressed() {
+        self.textView.resignFirstResponder()
+    }
+
+    private func configureLabelText() {
+        var text = self.labelText
+        if required {
+            text += " *"
+        }
+        self.label.text = text
+    }
+
+}
--- omni/View/PostForm.swift
+++ omni/View/PostForm.swift
@@ -6,4 +6,149 @@
 //  Copyright © 2018年 Rebecca Hsiao. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Cartography
+
+class PostEventForm: UIView {
+
+    private var stackView = UIStackView()
+    private var profileLineView = LabelAndTextInputView(frame: CGRect.zero)
+    private var from = LabelAndTextInputView(frame: CGRect.zero)
+    private var to = LabelAndTextInputView(frame: CGRect.zero)
+    private var location = LabelAndTextInputView(frame: CGRect.zero)
+    private var additionalNotes = LabelAndTextInputView(frame: CGRect.zero)
+
+    private var postType: PostType = .request
+    var post: Post?
+
+    init(post: Post?) {
+        self.post = post
+
+        super.init(frame: CGRect.zero)
+        if let post = self.post {
+            self.switchForm(postType: post.postType())
+            self.prepopulateForm(post: post)
+        }
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setup(width: CGFloat) {
+        self.stackView.axis = .vertical
+        self.stackView.alignment = .fill
+        self.addSubview(self.stackView)
+        self.switchForm(postType: .request)
+
+        constrain(self.stackView, self) { (stackView, selfView) in
+            stackView.edges == selfView.edges
+        }
+    }
+
+    func switchForm(postType: PostType) {
+        self.postType = postType
+
+        for subview in self.stackView.arrangedSubviews {
+            self.stackView.removeArrangedSubview(subview)
+        }
+
+        let toAndFromStackView = UIStackView()
+        toAndFromStackView.axis = .horizontal
+
+        self.profileLineView.labelText = "Topic"
+        self.from.labelText = "FROM (if applicable)"
+        self.to.labelText = "TO (if applicable)"
+        self.location.labelText = "LOCATION (if applicable)"
+        self.additionalNotes.labelText = "ADDITIONAL NOTES"
+        self.location.configureForPicker(options: ["My Place", "Your Place", "Other"])
+
+        self.additionalNotes.configureForFreeformInput()
+        self.to.configureForDateAndTimeInput()
+        self.from.configureForDateAndTimeInput()
+        self.to.configureNotTypable()
+        self.from.configureNotTypable()
+
+        self.profileLineView.addBorders(edges: .bottom)
+        self.to.addBorders(edges: .bottom)
+        self.from.addBorders(edges: .bottom)
+        self.from.addBorders(edges: .right)
+        self.location.addBorders(edges: .bottom)
+
+        var postTypeString = ""
+        switch postType {
+        case .request:
+            postTypeString = "request"
+        case .offer:
+            postTypeString = "offer"
+        }
+
+        self.profileLineView.placeholder = "Why do you want to \(postTypeString) this \(post?.item?.item_name ?? "item")?"
+        self.location.placeholder = "My Place, Your Place, Other"
+        self.additionalNotes.placeholder = "Anything you want to let the other part know"
+
+        self.stackView.translatesAutoresizingMaskIntoConstraints = false
+        self.stackView.addArrangedSubview(self.profileLineView)
+        toAndFromStackView.addArrangedSubview(self.from)
+        toAndFromStackView.addArrangedSubview(self.to)
+        toAndFromStackView.distribution = .fillEqually
+        self.stackView.addArrangedSubview(toAndFromStackView)
+        self.stackView.addArrangedSubview(self.location)
+        self.stackView.addArrangedSubview(self.additionalNotes)
+
+        let height = self.stackView.systemLayoutSizeFitting(UILayoutFittingCompressedSize).height
+        self.bounds.size = CGSize(width: self.bounds.width, height: height)
+    }
+
+    func validateField() -> Bool {
+        return self.from.validate() && self.location.validate()
+    }
+
+    func makePost() -> Post {
+        var post: Post! = self.post
+        if post == nil {
+            if self.postType == .request {
+                post = Request.createNew()
+            } else if self.postType == .offer {
+                post = Offer.createNew()
+            }
+        }
+        post?.text = self.profileLineView.text
+        post?.from = self.from.date
+        post?.to = self.to.date
+        post?.flexibleHours = self.from.flexibleHours ?? true
+        post?.location = self.location.text
+        post?.additionalNotes = self.additionalNotes.text
+
+        return post
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.stackView.frame = self.bounds
+    }
+
+    func prepopulateForm(post: Post) {
+
+        if let text = post.text {
+            self.profileLineView.textView.string = text
+        }
+        self.profileLineView.textView.delegate?.textViewDidChange?(self.profileLineView.textView)
+
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM/dd/yyyy"
+        if let from = post.from {
+            self.from.textView.string = formatter.string(from: from)
+        }
+        if let to = post.to {
+            self.to.textView.string = formatter.string(from: to)
+        }
+        if let location = post.location {
+            self.location.textView.string = location
+        }
+        if let notes = post.additionalNotes {
+            self.additionalNotes.textView.string = notes
+        }
+    }
+
+}
