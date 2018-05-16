eb175a4b1186d4c84fc9f1f5e92ac4ca2eb3711a
Becka <rebeccaellicehsiao@gmail.com>
2018-05-16 16:11:03 +0900

    Add in decorder function because doing custom initialization for EachItemPostsVC

--- omni/ViewController/EachItemPostsVC.swift
+++ omni/ViewController/EachItemPostsVC.swift
@@ -24,6 +24,10 @@ class EachItemPostsVC: UIViewController, UITableViewDataSource, UITableViewDeleg
         }
         self.navigationItem.title = item.item_name
     }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
