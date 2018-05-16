ef89b38d9e96680fcbdbd548fc6d525e53170413
Becka <rebeccaellicehsiao@gmail.com>
2018-05-16 15:50:23 +0900

    Add in custom fonts
    Add custom fonts to fonts folder and configured them in plist.info
    Also added in .gitnore file because pod files are starting to populate and it isn't necessarily to version control the builds of the pods.

--- Podfile
+++ Podfile
@@ -5,6 +5,8 @@ target 'omni' do
   use_frameworks!
 
   pod 'Alamofire', '~> 4.7'
+  pod 'Cartography', '~> 3.0.1'
+  pod 'Eureka', '~> 4.1.1'
 
 end
 
--- omni/Extensions/Font.swift
+++ omni/Extensions/Font.swift
@@ -11,12 +11,10 @@ import UIKit
 extension UIFont {
 
     class func normalFont(size: CGFloat = 12.0) -> UIFont {
-        print(nil) // TODO remember to import GothamPro when off the plane!
         return UIFont(name: "GothamPro", size: size)!
     }
 
     class func boldFont(size: CGFloat = 12.0) -> UIFont {
-        print(nil)
         return UIFont(name: "AvenirNext-Bold", size: size)!
     }
 
